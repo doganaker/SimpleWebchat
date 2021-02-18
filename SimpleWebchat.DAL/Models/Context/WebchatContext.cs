224fdba3ed43c4232722576eb4f3fda55cbff13b
Doğan Aker <m.doganaker@gmail.com>
2021-02-18 12:48:17 +0300

    Issue #2 completed, migrations done and database updated.

---
+++ SimpleWebchat.DAL/Models/Context/WebchatContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleWebchat.DAL.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWebchat.DAL.Models.Context
+{
+    class WebchatContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=localhost\SQLEXPRESS;database=WebchatDB;trusted_connection=true;");
+        }
+
+        public DbSet<AdminUser> AdminUsers { get; set; }
+    }
+}
---
+++ SimpleWebchat.DAL/Models/Entities/Chat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWebchat.DAL.Models.Entities
+{
+    class Chat : BaseEntity
+    {
+        public string CallerID { get; set; }
+        public string ClientID { get; set; }
+        public string Content { get; set; }
+
+        private string _time = DateTime.Now.ToString("HH:mm");
+        public string Time
+        {
+            get
+            {
+                return _time;
+            }
+            set
+            {
+                _time = value;
+            }
+        }
+    }
+}
