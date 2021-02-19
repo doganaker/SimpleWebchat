46bbebbad31492f6997826068ec8874e7607b2a4
Doğan Aker <m.doganaker@gmail.com>
2021-02-19 13:00:38 +0300

    SignalR implemented and WebchatHub added

--- SimpleWebchat.Web/Startup.cs
+++ SimpleWebchat.Web/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SimpleWebchat.Web.Hubs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,6 +36,7 @@ namespace SimpleWebchat.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute("default", "{Controller=Home}/{Action=Index}/{id?}");
+                endpoints.MapHub<WebchatHub>("/webchathub");
             });
         }
     }
