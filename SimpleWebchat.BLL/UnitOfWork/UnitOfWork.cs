13a37f33f9e9db2e802cfd68ce5808b516496665
Doğan Aker <m.doganaker@gmail.com>
2021-02-19 12:55:33 +0300

    Umit Of Work pattern added

---
+++ SimpleWebchat.BLL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using SimpleWebchat.BLL.Interfaces;
+using SimpleWebchat.DAL.Models.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWebchat.BLL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly WebchatContext _context;
+
+        public UnitOfWork(WebchatContext context)
+        {
+            _context = context;
+        }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public int SaveChanges()
+        {
+            try
+            {
+                return _context.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
